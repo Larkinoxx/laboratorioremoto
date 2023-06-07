7a1d6525f19f9e7243d4cfa45489284a12939b5f
maksilorak <maksilorak@gmail.com>
2023-06-07 00:08:55 -0500

    Creation of Login
    Created Login initial functionality using Material UI

---
+++ src/components/login/Login.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import { Box, Button,Container, Grid, Paper, TextField, Typography } from '@mui/material'
+
+export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+    
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email,password);
+    
+        // Validaciones básicas
+        if (!email || !password) {
+          setErrorMessage('Por favor, complete todos los campos');
+        } else if (password.length < 6) {
+          setErrorMessage('La contraseña debe tener al menos 6 caracteres');
+        } else {
+          setErrorMessage('');
+          // Aquí puedes enviar los datos de inicio de sesión al servidor
+        }
+    };
+
+    return (
+        <Container maxWidth='sm'>
+            <Grid
+                container direction={'colum'} 
+                alignItems={'center'} 
+                justifyContent={'center'}
+                sx={{minHeight:'100vh'}}
+            >
+                <Grid item xs={8}>
+                    <Paper sx={{padding:'1.2em', borderRadius:'0.5em'}}>
+                        <Typography 
+                            variant='h4'
+                            sx={{mt:1,mb:1}}
+                        >
+                            Iniciar Sesión
+                        </Typography>
+                        <Box component = 'form' onSubmit={handleSubmit}>
+                            <TextField
+                                name='username'
+                                fullWidth 
+                                label='Email'
+                                sx={{mt:2,mb:1.5}}
+                                required
+                                margin='normal'
+                                type='text'
+                                onChange={handleEmailChange}
+                            />
+                            <TextField 
+                                name='password'
+                                fullWidth 
+                                label='Contraseña'
+                                sx={{mt:2,mb:1.5}}
+                                required
+                                margin='normal'
+                                type='password'
+                                onChange={handlePasswordChange}
+                            />
+                            <Button 
+                                type='submit' 
+                                fullWidth
+                                variant='contained'
+                                sx={{mt:2,mb:3}}
+                            >
+                                Iniciar Sesión
+                            </Button>
+                        </Box>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Container>
+    )
+}
